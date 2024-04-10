ece4b070aa9c6b924eddc8c165b0864dc4b70745
Mayistikar <andersonrodriguezce@gmail.com>
2024-04-09 22:20:15 -0500

    adding user creationg

--- api.py
+++ api.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 # Postgresql connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/idrl'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app_context = app.app_context()
+app_context.push()
 db = SQLAlchemy(app)
 
 '''
@@ -34,12 +37,22 @@ class User(db.Model):
     name = db.Column(db.String(50))
     password = db.Column(db.String(50))
     user = db.Column(db.String(50))
+    email = db.Column(db.String(50))
 
 
 # To create a user
 @app.route('/users', methods=['POST'])
 def create_user():
-    return 'unimplemented', 501
+    new_user = User(
+        name=request.json['name'],
+        email=request.json['email'],
+        user=request.json['user'],
+        password=request.json['password'],
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created", "user": {"id": new_user.id, "name": new_user.name, "email": new_user.email}})
 
 
 '''
@@ -52,6 +65,10 @@ Video section
 def video():
     return 'unimplemented', 501
 
+
+# Inicializar Flask-Migrate
+db.create_all()
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
